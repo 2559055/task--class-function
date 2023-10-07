6c8b88733dd88c84bfa77d215df2f77c8af3e5ca
2559055 <mohamed.hussein@student.moringaschool.com>
2023-10-07 09:26:16 -0700

    task first class function

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+
+const drivers = ["Driver1", "Driver2", "Driver3", "Driver4"];
+
+
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+
+
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(-2);
+}
+
+
+function createFareMultiplier(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  };
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function selectDifferentDrivers(drivers, selectionFunction) {
+  return selectionFunction(drivers);
+}
+
+// Test the functions
+console.log(returnFirstTwoDrivers(drivers));
+console.log(returnLastTwoDrivers(drivers)); 
+console.log(fareDoubler(10)); 
+console.log(fareTripler(10));
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers)); 
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers)); 
